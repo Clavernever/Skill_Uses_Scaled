5b672abb306fbeb2dafbb712945fec5a509b10eb
Grey <git.enviably459@passinbox.com>
2024-02-18 11:24:13 -0300

    fix comment in weapon.lua

--- scripts/Skill_Uses_Scaled/weapon.lua
+++ scripts/Skill_Uses_Scaled/weapon.lua
@@ -6,7 +6,7 @@ local types = require('openmw.types')
 -- We don't use a more aggressive modifier because yes, we can make the player not notice...
 -- ...but going over 4X will risk crippling NPCs in long fights.
 -- I'm considering giving NPCs a constant weapon restoration effect, but I'm unsure about it.
--- Nevermind, this can be achieved by attaching this very same script to their weapons
+-- Nevermind, this CAN be done instead by attaching this very same script to their weapons
 -- Then sending events from an NPC script once/s to restore the same amount as func.lua does.
 -- Even though we can't use a SkillUsed handler, we _can_ just check their equipped weapon's durability.
 -- Of course, event calls should not happen while the NPC is under the effect of Disintegrate Weapon, for obvious reasons.
